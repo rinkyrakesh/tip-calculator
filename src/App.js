52ebe819024c31333853bf10261c3986a9d58ee4
Rinky Singh <rinsingh@publicisgroupe.net>
2025-07-16 12:23:40 +0530

    first update

--- src/App.js
+++ src/App.js
@@ -1,23 +1,74 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+
+function BillInput({ bill, setBill }) {
+  return (
+    <div>
+      <label>How much was the bill?</label>
+      <input
+        type="text"
+        placeholder="0"
+        value={bill}
+        onChange={(e) => setBill(Number(e.target.value))}
+      ></input>
+    </div>
+  );
+}
+console.log("bill");
+function ServicePercentage({ tip, setTip, children }) {
+  return (
+    <div>
+      <label>{children}</label>
+      <select value={tip} onChange={(e) => setTip(Number(e.target.value))}>
+        <option value="0">Dissatisfied (0%)</option>
+        <option value="5">it was OK (5%)</option>
+        <option value="10">it was good (10%)</option>
+        <option value="20">Absolutely Amazing (20%)</option>
+      </select>
+    </div>
+  );
+}
+function PayBill({ bill, totalPay, avgTip }) {
+  return (
+    <div>
+      <h3>
+        ` You pay ${totalPay} (${bill}+${avgTip} tip)`
+      </h3>
+    </div>
+  );
+}
+
+function Reset({ handleReset }) {
+  return <button onClick={handleReset}>Reset</button>;
+}
 
 function App() {
+  const [bill, setBill] = useState();
+  const [tip1, setTip1] = useState();
+  const [tip2, setTip2] = useState();
+  const avgTip = (tip1 + tip2) / 2;
+  const totalPay = bill + avgTip;
+
+  function handleReset() {
+    setBill("");
+    setTip1(0);
+    setTip2(0);
+  }
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <BillInput bill={bill} setBill={setBill} />
+      <ServicePercentage tip={tip1} setTip={setTip1}>
+        How did you like the service?
+      </ServicePercentage>
+      <ServicePercentage tip={tip2} setTip={setTip2}>
+        How did you friend like the service?
+      </ServicePercentage>
+      {bill > 0 && (
+        <>
+          <PayBill bill={bill} totalPay={totalPay} avgTip={avgTip} />
+          <Reset handleReset={handleReset} />
+        </>
+      )}
     </div>
   );
 }
